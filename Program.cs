4cb4f85ccb6a670197b978ec8e86607ef218d81b
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-11 11:15:14 +0530

    UC1-Added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace LinlListProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Program");
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+
+          
+        }
+    }
+}
